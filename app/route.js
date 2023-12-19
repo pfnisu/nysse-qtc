2110db2cdd64ec86d14ed715bd881f907a2fb47a
Niko Suoniemi <niko@tamperelainen.org>
2023-12-19 23:46:12 +0200

    Show patterns for routes

--- app/route.js
+++ app/route.js
@@ -8,19 +8,23 @@ export function Route() {
     this.compose = async () => {
         const rid = request.hash(this.title)
         const query = {
-            'query': `{ route(id: "tampere:${rid}") { longName stops { gtfsId name zoneId } } }`
+            'query': `{ route(id: "tampere:${rid}") {` +
+                'longName patterns { headsign stops { gtfsId name zoneId } } } }'
         }
         let json = await request.http(env.uri, 'POST', query, env.key)
         if (json) {
             document.title = `${rid} ${json.data.route.longName} | ${document.title}`
-            this.tree.innerHTML =
-                `<h2>${rid} ${json.data.route.longName}</h2><table><tbody></tbody></table>`
-            const content = this.tree.querySelector('tbody')
-            for (const stop of json.data.route.stops) {
-                const sid = stop.gtfsId.split(':')[1]
-                content.innerHTML += 
-                    `<tr><th class="zone">${stop.zoneId}</th><th class="stop">${sid}</th>` +
-                        `<td><a href="#p=1;stop=${sid}">${stop.name}</a></td></tr>`
+            this.tree.innerHTML = ''
+            for (const pattern of json.data.route.patterns) {
+                this.tree.innerHTML +=
+                    `<h2>${rid} &#8594; ${pattern.headsign}</h2><table><tbody></tbody></table>`
+                const content = this.tree.querySelector('table:last-of-type>tbody')
+                for (const stop of pattern.stops) {
+                    const sid = stop.gtfsId.split(':')[1]
+                    content.innerHTML +=
+                        `<tr><th class="zone">${stop.zoneId}</th><th class="stop">${sid}</th>` +
+                            `<td><a href="#p=1;stop=${sid}">${stop.name}</a></td></tr>`
+                }
             }
         } else this.tree.innerHTML = '<h2>Yhteysvirhe...</h2>'
     }
