46ee651f480d362dbbf670fef4eaf34d568c6b56
Niko Suoniemi <niko@tamperelainen.org>
2023-12-27 21:38:38 +0200

    Save alerts display-state to cookie

--- app/list.js
+++ app/list.js
@@ -12,10 +12,11 @@ export function List(listen) {
         }
         let json = await request.http(env.uri, 'POST', query, env.key)
         const hid = request.cookie('home')
+        const state = request.cookie('alerts')
         const home = hid ? `<a href="#p=1;stop=${hid}">Näytä kotipysäkki</a>` : ''
         this.tree.innerHTML =
             '<h2>Tampereen seudun joukkoliikenteen linjat</h2>' +
-            `<div style="display:none" id="alert"></div><div id="home"></div>` +
+            `<div${state === null ? '' : ' style="display:none"'} id="alert"></div><div id="home"></div>` +
             '<table><tbody></tbody></table>'
         const alerts = this.tree.querySelector('#alert')
         const content = this.tree.querySelector('tbody')
@@ -25,12 +26,13 @@ export function List(listen) {
             alerts.innerHTML = set.reduce((cat, a) => `${cat}<p>${a.alertDescriptionText}</p>`, '')
             const show = '&#8505; Näytä tiedotteet'
             const hide = '&#10005; Sulje tiedotteet'
-            const toggle = alerts.innerHTML ? `<button>${show}</button><br/>` : ''
+            const toggle = alerts.innerHTML ? `<button>${state === null ? hide : show}</button><br/>` : ''
             this.tree.querySelector('#home').innerHTML = `${toggle}${home}`
             this.tree.querySelector('button')?.addEventListener('click', (ev) => {
                 const a = this.tree.querySelector('#alert')
                 a.style.display = a.style.display === 'none' ? '' : 'none'
                 ev.target.innerHTML = a.style.display === 'none' ? show : hide
+                request.cookie('alerts', a.style.display)
             })
             json.data.routes.sort((a, b) => parseInt(a.shortName) - parseInt(b.shortName))
             for (const route of json.data.routes)
