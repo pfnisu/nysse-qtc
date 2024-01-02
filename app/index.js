b5a18be835daf690afa68181e7c43e42fa6c914b
Niko Suoniemi <niko@tamperelainen.org>
2024-01-02 20:52:30 +0200

    Implement lang toggle

--- app/index.js
+++ app/index.js
@@ -2,15 +2,18 @@ import ui from './lib/ui.js'
 import request from './lib/request.js'
 import {Lines} from './lines.js'
 import {Stops} from './stops.js'
-import {Info} from './info.js'
+import {Settings} from './settings.js'
 
 // Language object
 const l = {}
+
 const main = async () => {
     l.str = await request.http(`lang/${request.cookie('lang') ?? 'fi'}.json`)
+    const settings = new Settings(l)
+    l.listen = settings.listen
     const stops = new Stops(l)
     ui.bind(
-        [new Lines(l, stops.listen), stops, new Info(l)],
+        [new Lines(l, stops.listen), stops, settings],
         document.querySelector('main'),
         document.querySelector('nav'),
         l.str.title)
--- app/settings.js
+++ app/settings.js
@@ -1,9 +1,14 @@
 import ui from './lib/ui.js'
+import request from './lib/request.js'
 
-export function Info(l) {
-    ui.init(this, 'Info', false)
+export function Settings(l) {
+    ui.init(this, l.str.settings)
+
     this.compose = async () => {
+        const lang = request.cookie('lang') ?? 'fi'
         this.tree.innerHTML =
+            '<h2>Kieli/Language</h2>' +
+            `<p><button id="fi">suomi</button> <button id="en">english</button></p>` +
             `<h2>${l.str.dev}</h2>` +
             `<p>${l.str.src}: <a href="https://github.com/pfnisu/nysse-qtc/">https://github.com/pfnisu/nysse-qtc/</a></p>` +
             `<p>${l.str.api}: <a href="https://digitransit.fi/">https://digitransit.fi/</a></p>` +
@@ -12,5 +17,15 @@ export function Info(l) {
             '<p>This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License as published by the Free Software Foundation, version 3.</p>' +
             '<p>This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more details.</p>' +
             '<p>You should have received a copy of the GNU Affero General Public License along with this program. If not, see <a href="https://www.gnu.org/licenses/">https://www.gnu.org/licenses/</a></p>'
+        this.tree.querySelector(`#${lang}`).innerHTML += " &#10003;"
+        this.tree.querySelector('p').addEventListener('click', async (ev) => {
+            if (ev.target.id) {
+                request.cookie('lang', ev.target.id)
+                l.str = await request.http(`lang/${ev.target.id}.json`)
+                this.title = l.str.settings
+                this.notify()
+                window.dispatchEvent(new Event('hashchange'))
+            }
+        }, true)
     }
 }
