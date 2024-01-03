b04a49261450808d81210b1cf9b7e7b6c190306a
Niko Suoniemi <niko@tamperelainen.org>
2024-01-03 18:31:43 +0200

    Improve licensing details

--- app/settings.js
+++ app/settings.js
@@ -11,12 +11,12 @@ export function Settings(l) {
             `<p><button id="fi">suomi</button> <button id="en">english</button></p>` +
             `<h2>${l.str.dev}</h2>` +
             `<p>${l.str.src}: <a href="https://github.com/pfnisu/nysse-qtc/">https://github.com/pfnisu/nysse-qtc/</a></p>` +
-            `<p>${l.str.api}: <a href="https://digitransit.fi/">https://digitransit.fi/</a></p>` +
+            `<p>${l.str.api}: &copy; <a href="https://digitransit.fi/en/developers/apis/6-terms-of-use/">Digitransit</a> 2024</p>` +
             `<h2>${l.str.license}</h2>` +
-            '<p>Copyright (C) 2023-2024 Niko Suoniemi &lt;niko@tamperelainen.org&gt;</p>' +
+            '<p>Copyright &copy; 2023-2024 Niko Suoniemi &lt;niko@tamperelainen.org&gt;</p>' +
             '<p>This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License as published by the Free Software Foundation, version 3.</p>' +
             '<p>This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more details.</p>' +
-            '<p>You should have received a copy of the GNU Affero General Public License along with this program. If not, see <a href="https://www.gnu.org/licenses/">https://www.gnu.org/licenses/</a></p>'
+            '<p>You should have received a copy of the GNU Affero General Public License along with this program. If not, see &lt;<a href="https://www.gnu.org/licenses/">https://www.gnu.org/licenses/</a>&gt;.</p>'
         this.tree.querySelector(`#${lang}`).innerHTML += " &#10003;"
         this.tree.querySelector('p').addEventListener('click', async (ev) => {
             if (ev.target.id) {
