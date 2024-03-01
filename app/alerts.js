84c54e3ed5ba7e6eba71ee5e29134acb70f197d7
Niko Suoniemi <niko@tamperelainen.org>
2024-03-01 18:24:30 +0200

    Clean up code structure

--- app/alerts.js
+++ app/alerts.js
@@ -6,21 +6,14 @@ export function Alerts(l, listenLang, listenHome, set) {
     ui.init(this, 'alerts')
 
     this.compose = async () => {
+        const state = request.cookie('alerts')
+        const lang = request.cookie('lang') || 'fi'
         const hid = request.cookie('home')
         const did = request.cookie('dest')
-        const state = request.cookie('alerts')
-        const home = hid
-            ? `<li><a href="#p=1;stop=${hid}">${l.str.goHome}</a></li>`
-            : ''
-        const dest = did
-            ? `<li><a href="#p=1;stop=${did}">${l.str.goDest}</a></li>`
-            : ''
         this.tree.innerHTML =
             `<div${state === null ? '' : ' class="hidden"'} id="alert"></div>` +
             '<ul></ul>'
-        let html = ''
         const alerts = $('#alert', this)
-        const lang = request.cookie('lang') || 'fi'
         alerts.innerHTML = set.reduce((cat, a) => {
             const severity = a[a.length - 1] === 'SEVERE' ? ' class="severe"' : ''
             const t = a.find((t) => t.language === lang)
@@ -29,6 +22,12 @@ export function Alerts(l, listenLang, listenHome, set) {
         }, '')
         const toggle = alerts.innerHTML
             ? `<li><button>${state ? l.str.open : l.str.close}</button></li>`
+            : ''
+        const home = hid
+            ? `<li><a href="#p=1;stop=${hid}">${l.str.goHome}</a></li>`
+            : ''
+        const dest = did
+            ? `<li><a href="#p=1;stop=${did}">${l.str.goDest}</a></li>`
             : ''
         $('ul', this).innerHTML = `${toggle}${home}${dest}`
         // Tree gets overwritten on re-compose, listener can be GC'd
