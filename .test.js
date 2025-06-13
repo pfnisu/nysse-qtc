7c059a5616ae13a2a841089fdbdb8ec256864822
Niko Suoniemi <pfnisu@outlook.com>
2025-06-13 17:31:00 +0300

    Add test build config

--- .test.js
+++ .test.js
@@ -2,8 +2,8 @@ require('esbuild').build({
     entryPoints: ['app/index.js','res/index.css'],
     outdir: 'build/',
     bundle: true,
-    minify: true,
+    minify: false,
     mangleProps: /^_/,
     target: 'es2017',
-    define: { test: 'false' },
+    define: { test: 'true' },
 }).catch(() => process.exit(1))
