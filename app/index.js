d17269eee583c71c191fc837657c0891b6c1805c
Niko Suoniemi <pfnisu@outlook.com>
2025-07-27 17:41:09 +0300

    Init Timetable earlier to fix hl regression

--- app/index.js
+++ app/index.js
@@ -3,6 +3,7 @@ import request from './lib/request.js'
 import {Lines} from './lines.js'
 import {Route} from './route.js'
 import {Stops} from './stops.js'
+import {Timetable} from './timetable.js'
 import {Settings} from './settings.js'
 
 (async () => {
@@ -17,10 +18,10 @@ import {Settings} from './settings.js'
     const size = request.cookie('size')
     if (size) document.documentElement.style.setProperty('--size', size)
 
-    // Route is initialized here so that pid listener works
+    // Route and Timetable are initialized here so that listeners work
     // with every app entry point
     ui.bind(
-        [new Lines(l, new Route(l)), new Stops(l), new Settings(l)],
+        [new Lines(l, new Route(l)), new Stops(l, new Timetable(l)), new Settings(l)],
         $('main'),
         $('nav'),
         ' | ' + document.title)
--- app/stops.js
+++ app/stops.js
@@ -1,13 +1,12 @@
 import ui from './lib/ui.js'
 import {Search} from './search.js'
-import {Timetable} from './timetable.js'
 
 // Parent view of Search and Timetable
-export function Stops(l) {
+export function Stops(l, timetable) {
     ui.init(this, l.str.stops)
 
     this.load = async () => {
-        ui.bind([new Search(l), new Timetable(l)], this.tree)
+        ui.bind([new Search(l), timetable], this.tree)
     }
 
     ui.listen('lang', () => this.name = l.str.stops)
